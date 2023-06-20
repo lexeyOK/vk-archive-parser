d054f75116458aa157f51a1a3ac930ad73dae940
lexx <35109763+lexeyOK@users.noreply.github.com>
2023-06-20 23:52:59 +0500

    add command to change charset

--- utils.sh
+++ utils.sh
@@ -2,6 +2,6 @@ mkdir vk;cd vk
 unzip Archive.zip
 mkdir -p ../vk_utf8
 fd -t d -x mkdir -p ../vk_utf8/{}
-fd --threads=4 -e html -x iconv -f WINDOWS-1251 -t UTF-8 {} -o ../vk_utf8/{.}.html
-
+fd -e html -x iconv -f WINDOWS-1251 -t UTF-8 {} -o ../vk_utf8/{.}.html
+fd -e html -x sed -i 's/windows-1251/utf-8/g' {}
 rg "attachment__description\">\D" --no-filename --no-line-number | awk '! a[$0]++'
