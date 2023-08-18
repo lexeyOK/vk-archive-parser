bb5cc6f93b90df0b0537b2ab8bd6b69d1d04a368
lexx <35109763+lexeyOK@users.noreply.github.com>
2023-08-18 20:44:41 +0500

    changed handeling of errors with anyhow::Result changed preatty printed json to minified

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod vk_chat;
+use anyhow;
 use std::{
     fs::File,
     io::{BufWriter, Write},
@@ -6,9 +6,10 @@ use std::{
     time::Instant,
 };
 
+mod vk_chat;
 use vk_chat::{join_pages, parse_pages};
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let started = Instant::now();
 
     let folder = std::env::args()
@@ -18,22 +19,17 @@ fn main() {
     let path = Path::new(&folder);
     print!("{:?} ", &folder);
 
-    let id: isize = path
-        .file_name()
-        .unwrap()
-        .to_str()
-        .unwrap()
-        .parse::<_>()
-        .unwrap();
+    let id: isize = path.file_name().unwrap().to_str().unwrap().parse::<_>()?;
 
-    let pages = parse_pages(path);
+    let pages = parse_pages(path)?;
     let chat = join_pages(&pages, id);
 
-    let data_file = File::create(format!("{}.json", chat.id)).unwrap();
+    let data_file = File::create(format!("{}.json", chat.id))?;
     let mut writer = BufWriter::new(data_file);
 
-    let serialised = serde_json::to_string_pretty(&chat).expect("you have fuckted up");
-    writer.write_all(serialised.as_ref()).unwrap();
+    let serialised = serde_json::to_string(&chat)?;
+    writer.write_all(serialised.as_ref())?;
 
     println!("Done in {} s!", started.elapsed().as_secs_f32());
+    Ok(())
 }
--- src/vk_chat.rs
+++ src/vk_chat.rs
@@ -1,11 +1,11 @@
 use aho_corasick::AhoCorasick;
+use anyhow::{Context, Result};
 use chrono::NaiveDateTime;
 use encoding_rs::WINDOWS_1251;
 use encoding_rs_io::DecodeReaderBytesBuilder;
 use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle};
 use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
-use tl::ParserOptions;
 
 use std::{
     collections::HashSet,
@@ -14,6 +14,7 @@ use std::{
     path::Path,
     sync::OnceLock,
 };
+use tl::ParserOptions;
 
 const TIME_ZONE_CORRECTION: i64 = 5 * 3600; // one hour is 3600 seconds
 const SELF_ID_URL: &str = "https://vk.com/id0";
@@ -71,9 +72,8 @@ pub fn join_pages(pages: &[VkPage], id: isize) -> VkChat {
 }
 
 /// Parse chat folder.
-pub fn parse_pages(folder: impl AsRef<Path>) -> Vec<VkPage> {
-    let file_paths = std::fs::read_dir(&folder)
-        .expect("access denied")
+pub fn parse_pages(folder: impl AsRef<Path>) -> anyhow::Result<Vec<VkPage>> {
+    let file_paths = std::fs::read_dir(&folder)?
         .filter_map(|entry| entry.ok().map(|entry| entry.path()))
         .filter(|path| path.is_file())
         .collect::<Vec<_>>();
@@ -87,28 +87,28 @@ pub fn parse_pages(folder: impl AsRef<Path>) -> Vec<VkPage> {
         .par_iter()
         .progress_with(ProgressBar::new(file_paths.len() as u64).with_style(progress_style))
         .map(|file_path| {
-            let text = decode_text(file_path);
+            let text = decode_text(file_path)?;
             parse_text(&text)
         })
-        .collect::<Vec<VkPage>>();
+        .collect::<Result<Vec<VkPage>>>()?;
     pages.sort_by(|a, b| a.page_number.cmp(&b.page_number));
-    pages
+    Ok(pages)
 }
 
 /// Decode text from file in cp1251 encoding.
-fn decode_text(file_path: impl AsRef<Path>) -> String {
+fn decode_text(file_path: impl AsRef<Path>) -> anyhow::Result<String> {
     let file = File::open(file_path).unwrap();
     let file = DecodeReaderBytesBuilder::new()
         .encoding(Some(WINDOWS_1251))
         .build(file);
     let mut buffer = BufReader::new(file);
     let mut text = String::new();
-    buffer.read_to_string(&mut text).unwrap();
-    text
+    buffer.read_to_string(&mut text)?;
+    Ok(text)
 }
 
 /// Parse html page.
-fn parse_text(input: &str) -> VkPage {
+fn parse_text(input: &str) -> anyhow::Result<VkPage> {
     let dom = tl::parse(input, ParserOptions::default()).unwrap();
     let parser = dom.parser();
     let messages = dom.get_elements_by_class_name("message");
@@ -116,7 +116,7 @@ fn parse_text(input: &str) -> VkPage {
     let message_items = messages
         .map(|message| message.get(parser).unwrap().as_tag().unwrap())
         .map(|message| parse_message(message, parser))
-        .collect::<Vec<_>>();
+        .collect::<anyhow::Result<Vec<_>>>()?;
 
     let page_number: usize = match dom.get_elements_by_class_name("pg_lnk_sel").next() {
         Some(link) => link
@@ -128,22 +128,21 @@ fn parse_text(input: &str) -> VkPage {
         None => 1,
     };
 
-    VkPage {
+    Ok(VkPage {
         page_number,
         message_items,
-    }
+    })
 }
 
 /// Parse message.
-fn parse_message(message: &tl::HTMLTag<'_>, parser: &tl::Parser) -> Message {
+fn parse_message(message: &tl::HTMLTag<'_>, parser: &tl::Parser) -> anyhow::Result<Message> {
     let id: usize = message // item_nodes[3]
         .attributes()
         .get("data-id")
         .unwrap()
         .unwrap()
         .as_utf8_str()
-        .parse()
-        .unwrap();
+        .parse()?;
     let header = message
         .query_selector(parser, ".message__header")
         .unwrap()
@@ -169,7 +168,7 @@ fn parse_message(message: &tl::HTMLTag<'_>, parser: &tl::Parser) -> Message {
             None => std::borrow::Cow::Borrowed(SELF_ID_URL),
         };
         let slug_str = link_href.split_at(15).1;
-        parse_from_id(slug_str)
+        parse_from_id(slug_str)?
     };
 
     let message_inner_text = message.inner_text(parser);
@@ -177,7 +176,7 @@ fn parse_message(message: &tl::HTMLTag<'_>, parser: &tl::Parser) -> Message {
     let date = {
         let header_str = split_message[1];
         let date_str = header_str.rsplit_once(", ").unwrap().1;
-        parse_date_time(date_str) + TIME_ZONE_CORRECTION
+        parse_date_time(date_str)? + TIME_ZONE_CORRECTION
     };
 
     let message_text = split_message[2].trim().to_string();
@@ -187,13 +186,13 @@ fn parse_message(message: &tl::HTMLTag<'_>, parser: &tl::Parser) -> Message {
             .collect()
     });
 
-    Message {
+    Ok(Message {
         id,
         from_id,
         date,
         message_text,
         attachments,
-    }
+    })
 }
 
 fn parse_attachment(item: &tl::HTMLTag<'_>, parser: &tl::Parser) -> Attachment {
@@ -213,39 +212,44 @@ fn parse_attachment(item: &tl::HTMLTag<'_>, parser: &tl::Parser) -> Attachment {
     Attachment { description, link }
 }
 
-fn parse_from_id(slug_str: &str) -> isize {
-    let (tp, from_id_str) = slug_str.split_at(slug_str.find(|c| char::is_ascii_digit(&c)).unwrap());
+fn parse_from_id(slug_str: &str) -> anyhow::Result<isize> {
+    let (tp, from_id_str) = slug_str.split_at(
+        slug_str
+            .find(|c| char::is_ascii_digit(&c))
+            .with_context(|| format!("wrong slug: {}", slug_str))?,
+    );
     let sign = if matches!(tp, "club" | "public") {
         -1
     } else {
         1
     };
-    sign * from_id_str.parse::<isize>().unwrap()
+    Ok(sign * from_id_str.parse::<isize>()?)
 }
 
 #[test]
 fn simple_from_id() {
-    assert_eq!(parse_from_id(SELF_ID_URL.split_at(15).1), 0);
-    assert_eq!(parse_from_id("club1"), -1);
+    assert_eq!(parse_from_id(SELF_ID_URL.split_at(15).1).unwrap(), 0);
+    assert_eq!(parse_from_id("club1").unwrap(), -1);
 }
 
 static AC: OnceLock<AhoCorasick> = OnceLock::new();
 
-fn parse_date_time(date_str: &str) -> i64 {
+fn parse_date_time(date_str: &str) -> anyhow::Result<i64> {
     let ac = AC.get_or_init(|| {
         AhoCorasick::new([
             "янв", "фев", "мар", "апр", "мая", "июн", "июл", "авг", "сен", "окт", "ноя", "дек",
         ])
-        .unwrap()
+        .expect("walid haystack")
     });
     let replace_with = &[
         "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12",
     ];
     let result = ac.replace_all(date_str, replace_with);
-    NaiveDateTime::parse_and_remainder(&result, "%d %m %Y в %H:%M:%S")
-        .unwrap_or_else(|_| panic!("this is not a valid date time: {}", &result))
+    let timestamp = NaiveDateTime::parse_and_remainder(&result, "%d %m %Y в %H:%M:%S")
+        .with_context(|| format!("this is not a valid date time: {}", &result))?
         .0
-        .timestamp()
+        .timestamp();
+    Ok(timestamp)
 }
 
 #[test]
@@ -257,5 +261,5 @@ fn simple_date_time() {
         .and_hms_opt(8, 34, 0)
         .unwrap()
         .timestamp();
-    assert_eq!(parse_date_time(string), dt);
+    assert_eq!(parse_date_time(string).unwrap(), dt);
 }
